7f0792c7d7cdf53c6ed286aa1f2a9a94979971e0
hechaoqi123 <1127181497@qq.com>
2019-06-08 11:09:30 +0800

    初始化jinkens项目

--- jinkens2/src/main/java/com/study/jinkens/JinkensApplication.java
+++ jinkens2/src/main/java/com/study/jinkens/JinkensApplication.java
@@ -2,12 +2,19 @@ package com.study.jinkens;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class JinkensApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JinkensApplication.class, args);
     }
-
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello world!";
+    }
 }
