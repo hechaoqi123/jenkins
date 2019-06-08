c412003148e7155bd37b3f01e83e698eba04217a
hechaoqi123 <1127181497@qq.com>
2019-06-09 00:11:55 +0800

    取消SpringBoot默认Logo

--- jinkens2/src/main/java/com/study/jinkens/JinkensApplication.java
+++ jinkens2/src/main/java/com/study/jinkens/JinkensApplication.java
@@ -1,18 +1,14 @@
 package com.study.jinkens;
-
+import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
 
 @SpringBootApplication
 public class JinkensApplication {
     public static void main(String[] args) {
-        SpringApplication.run(JinkensApplication.class, args);
+        SpringApplication app=new SpringApplication(JinkensApplication.class);
+         app.setBannerMode(Banner.Mode.OFF);//取消springBoot的默认logo
+         app.run(args);
     }
 
 }
