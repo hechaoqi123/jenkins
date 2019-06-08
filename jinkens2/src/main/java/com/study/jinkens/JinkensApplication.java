63feef52952730cd5a68e77ebedff0bfd94f13e1
hechaoqi123 <1127181497@qq.com>
2019-06-08 10:57:06 +0800

    初始化jinkens项目

---
+++ jinkens2/src/main/java/com/study/jinkens/JinkensApplication.java
@@ -0,0 +1,13 @@
+package com.study.jinkens;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JinkensApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JinkensApplication.class, args);
+    }
+
+}
